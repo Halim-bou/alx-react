997b4bac4ae103e02244594781b89184061946bf
aelbouaa <hbouaami@gmail.com>
2024-06-24 21:37:07 +0100

    task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  performance: {
+    hints: 'warning',
+    maxAssetSize: 200000, // 200kb
+    maxEntrypointSize: 400000, // 400kb
+  },
+};
+
